a92f6c8485c617dcba399b99050399dc599e1674
Simonek, Jan <jsimonek@netsuite.com>
2022-03-23 13:28:29 +0100

    Logging on URL /zoomhook

--- src/main/java/online/kancl/Main.java
+++ src/main/java/online/kancl/Main.java
@@ -1,6 +1,10 @@
 package online.kancl;
 
+import spark.Request;
+import spark.Response;
 import spark.Spark;
+
+import java.util.stream.Collectors;
 
 public class Main
 {
@@ -10,7 +14,19 @@ public class Main
 		Spark.init();
 
 		Spark.get("/", (req, res) -> "Hello World");
+		Spark.get("/zoomhook", Main::zoomHook);
+		Spark.post("/zoomhook", Main::zoomHook);
 
 		System.out.println("Server running");
+	}
+
+	private static Object zoomHook(Request request, Response response)
+	{
+		String queryParams = request.queryParams().stream().map(key -> key + "=" + String.join(",", request.queryParamsValues(key)))
+				.collect(Collectors.joining("&"));
+
+		System.out.println(request.requestMethod() + " /zoomhook?" + queryParams + " " + request.body());
+
+		return "OK";
 	}
 }
