2a9f2e6464e65766077ba0c2218f38696271d337
Simonek, Jan <jsimonek@netsuite.com>
2022-07-08 15:40:52 +0200

    Added unit test MeetingsTest

---
+++ src/test/java/online/kancl/MeetingsTest.java
@@ -0,0 +1,19 @@
+package online.kancl;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MeetingsTest
+{
+	private final Meetings meetings = new Meetings();
+
+	@Test
+	void joinedParticipant_isInMeeting()
+	{
+		meetings.participantJoined("John Doe");
+
+		Assertions.assertThat(meetings.getJoinedParticipantName())
+				.get()
+				.isEqualTo("John Doe");
+	}
+}
