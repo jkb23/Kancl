bb440f5466be7f79be2dcb5b1fad0ab50d35e635
Simonek, Jan <jsimonek@netsuite.com>
2022-07-09 21:25:47 +0200

    fix previous

---
+++ run/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# exit script on first error
+set -e
+
+function onError()
+{
+  echo -e
+  echo -e "${RED}Failed${NC}"
+}
+trap onError ERR
+
+echo ""
+echo "Deploying"
+echo "-------------"
+echo ""
+
+docker stop $(docker ps -q) | echo ""
+docker build -f run/Dockerfile --tag kancl-online .
+docker run -dt -p 80 -e DOMAIN="$DOMAIN" --name kancl-online
+docker container prune -f
+
+echo -e ""
+echo -e "${GREEN}Success${NC}"
