9ea6afb44195e8252f6342b47d62ef065fdbe374
Simonek, Jan <jsimonek@netsuite.com>
2022-07-12 22:06:59 +0200

    attempt to fix gitlab-ci.yml

--- run/deploy.sh
+++ run/deploy.sh
@@ -24,7 +24,9 @@ docker stop $(docker ps -q) | echo ""
 docker volume rm sql_data | echo ""
 docker volume create --name=sql_data | echo ""
 
+cd run
 docker-compose -f docker-compose.yml -f docker-compose-prod.yml up --build --exit-code-from cypress
+cd ..
 
 docker container prune -f
 
