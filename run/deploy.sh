03e90569e4fcc714e0df963a46d2521a7e4e2305
Simonek, Jan <jsimonek@netsuite.com>
2022-07-12 22:19:16 +0200

    removing cypress from deploy.sh

--- run/deploy.sh
+++ run/deploy.sh
@@ -25,7 +25,7 @@ docker volume rm sql_data | echo ""
 docker volume create --name=sql_data | echo ""
 
 cd run
-docker-compose -f docker-compose.yml -f docker-compose-prod.yml up --build --exit-code-from cypress
+docker-compose -f docker-compose.yml up --build
 cd ..
 
 docker container prune -f
