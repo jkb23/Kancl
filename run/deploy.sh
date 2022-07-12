278df7b710b8fb51ee203a891db226fb6d44bbff
Simonek, Jan <jsimonek@netsuite.com>
2022-07-12 22:41:07 +0200

    readding --detach in deploy.sh

--- run/deploy.sh
+++ run/deploy.sh
@@ -25,7 +25,7 @@ docker volume rm sql_data | echo ""
 docker volume create --name=sql_data | echo ""
 
 cd run
-docker-compose -f docker-compose.yml -f docker-compose-prod.yml up --build
+docker-compose -f docker-compose.yml -f docker-compose-prod.yml up --build --detach
 cd ..
 
 docker container prune -f
