2db49cf1ad48cdbd3e6d2fcebcfd71413966acbd
Simonek, Jan <jsimonek@netsuite.com>
2022-08-05 08:09:49 +0200

    MainPageController extracted getTemplateFile

--- src/main/java/online/kancl/controller/MainPageController.java
+++ src/main/java/online/kancl/controller/MainPageController.java
@@ -6,13 +6,17 @@ import online.kancl.Meetings;
 import spark.Request;
 import spark.Response;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class MainPageController
 {
+	public static final Path TEMPLATE_DIRECTORY = Paths.get("web", "templates");
+
 	private final MustacheFactory mustacheFactory;
 	private final Meetings meetings;
 
@@ -24,7 +28,7 @@ public class MainPageController
 
 	public String get(Request request, Response response)
 	{
-		try (FileReader fileReader = new FileReader(Paths.get("web", "templates", "MainPage.mustache").toFile()))
+		try (FileReader fileReader = new FileReader(getTemplateFile("MainPage.mustache")))
 		{
 			Mustache template = mustacheFactory.compile(fileReader, "MainPage");
 			return template.execute(new StringWriter(), meetings).toString();
@@ -33,5 +37,10 @@ public class MainPageController
 		{
 			throw new RuntimeException(e);
 		}
+	}
+
+	private File getTemplateFile(String templateName)
+	{
+		return TEMPLATE_DIRECTORY.resolve(templateName).toFile();
 	}
 }
