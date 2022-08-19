2611338b7551812fc39862b0cbf6a67c6093d2a0
Simonek, Jan <jsimonek@netsuite.com>
2022-08-19 09:28:06 +0200

    Fixing cypress.config.js to JS syntax

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'cypress'
+const { defineConfig } = require('cypress')
 
-export default defineConfig({
-    pageLoadTimeout: 10000,
-    responseTimeout: 5000,
-    videosFolder: 'src/test/cypress/videos',
-    screenshotsFolder: 'src/test/cypress/screenshots',
-    e2e: {
-        specPattern: "src/test/cypress/e2e/**/*.ts",
-        supportFile: false,
-        baseUrl: "http://localhost:8081"
-    }
+module.exports = defineConfig({
+	pageLoadTimeout: 10000,
+	responseTimeout: 5000,
+	videosFolder: 'src/test/cypress/videos',
+	screenshotsFolder: 'src/test/cypress/screenshots',
+	e2e: {
+		specPattern: "src/test/cypress/e2e/**/*.ts",
+		supportFile: false,
+		baseUrl: "http://localhost:8081"
+	}
 });
