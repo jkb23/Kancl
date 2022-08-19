92f19cdd25f34f0a90c3a524e160e07d4888a944
Simonek, Jan <jsimonek@netsuite.com>
2022-08-19 11:34:02 +0200

    Moved PebbleExtension custom filter and test as inner classes

--- src/main/java/online/kancl/server/template/PebbleExtension.java
+++ src/main/java/online/kancl/server/template/PebbleExtension.java
@@ -1,17 +1,22 @@
 package online.kancl.server.template;
 
 import com.mitchellbosecke.pebble.attributes.AttributeResolver;
+import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.extension.Extension;
 import com.mitchellbosecke.pebble.extension.Filter;
 import com.mitchellbosecke.pebble.extension.Function;
 import com.mitchellbosecke.pebble.extension.NodeVisitorFactory;
 import com.mitchellbosecke.pebble.extension.Test;
+import com.mitchellbosecke.pebble.extension.core.EmptyTest;
 import com.mitchellbosecke.pebble.operator.BinaryOperator;
 import com.mitchellbosecke.pebble.operator.UnaryOperator;
+import com.mitchellbosecke.pebble.template.EvaluationContext;
+import com.mitchellbosecke.pebble.template.PebbleTemplate;
 import com.mitchellbosecke.pebble.tokenParser.TokenParser;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class PebbleExtension implements Extension
 {
@@ -67,5 +72,66 @@ public class PebbleExtension implements Extension
 	public List<AttributeResolver> getAttributeResolver()
 	{
 		return null;
+	}
+
+	private static class OrElseFilter implements Filter
+	{
+		@Override
+		public Object apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) throws PebbleException
+		{
+			Object defaultValue = args.get("default");
+
+			Test emptyTest = new EmptyOptionalTest();
+			boolean isEmpty = emptyTest.apply(input, args, self, context, lineNumber);
+
+			if (isEmpty)
+			{
+				return defaultValue;
+			}
+			else
+			{
+				if (input instanceof Optional)
+				{
+					return ((Optional<?>) input).orElse(null);
+				}
+				else
+				{
+					return input;
+				}
+			}
+		}
+
+		@Override
+		public List<String> getArgumentNames()
+		{
+			return List.of("default");
+		}
+	}
+
+	private static class EmptyOptionalTest implements Test
+	{
+		@Override
+		public boolean apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) throws PebbleException
+		{
+			EmptyTest emptyTest = new EmptyTest();
+			if (emptyTest.apply(input, args, self, context, lineNumber))
+			{
+				return true;
+			}
+			else if (input instanceof Optional)
+			{
+				return ((Optional<?>) input).isEmpty();
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		@Override
+		public List<String> getArgumentNames()
+		{
+			return null;
+		}
 	}
 }
