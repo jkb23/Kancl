0b6a646deaffd1f5ef7f5d2e0a2589d46e4ffa6d
Simonek, Jan <jsimonek@netsuite.com>
2022-08-21 22:10:27 +0200

    Reformatted PebbleTemplateRendererTest and simplified getTemplateDirectory()

--- src/test/java/online/kancl/server/template/PebbleTemplateRendererTest.java
+++ src/test/java/online/kancl/server/template/PebbleTemplateRendererTest.java
@@ -10,52 +10,45 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class PebbleTemplateRendererTest
-{
+class PebbleTemplateRendererTest {
 	public static final String TEST_TEMPLATE_DIRECTORY = "template";
 
 	private final Context context = new Context();
 
 	@Test
-	void singleValueTest()
-	{
+	void singleValueTest() {
 		assertThat(renderTemplate("singleValue.peb", context))
 				.contains("Hello John Doe!");
 	}
 
 	@Test
-	void partialValue()
-	{
+	void partialValue() {
 		assertThat(renderTemplate("extends.peb", context))
 				.isEqualTo("Hello John Doe! How are you, John Doe?");
 	}
 
 	@Test
-	void emptyOptionalTest()
-	{
+	void emptyOptionalTest() {
 		assertThat(renderTemplate("optional.peb", context))
 				.isEqualTo("default value");
 	}
 
 	@Test
-	void filledOptionalTest()
-	{
+	void filledOptionalTest() {
 		context.optional = Optional.of("Foo");
 
 		assertThat(renderTemplate("optional.peb", context))
 				.isEqualTo("Foo");
 	}
 
-	private String renderTemplate(String templateName, Object context)
-	{
-		PebbleTemplateRenderer renderer = createPebbleTemplateRenderer(templateName);
+	private String renderTemplate(String templateName, Object context) {
+		PebbleTemplateRenderer renderer = createPebbleTemplateRenderer();
 		return renderer.renderTemplate(templateName, context);
 	}
 
-	private PebbleTemplateRenderer createPebbleTemplateRenderer(String templateName)
-	{
+	private PebbleTemplateRenderer createPebbleTemplateRenderer() {
 		var pebbleTemplateLoader = new FileLoader();
-		pebbleTemplateLoader.setPrefix(getTemplateDirectory(templateName).toAbsolutePath().toString());
+		pebbleTemplateLoader.setPrefix(getTemplateDirectory().toAbsolutePath().toString());
 		var pebbleEngine = new PebbleEngine.Builder()
 				.loader(pebbleTemplateLoader)
 				.extension(new PebbleExtension())
@@ -63,20 +56,18 @@ class PebbleTemplateRendererTest
 		return new PebbleTemplateRenderer(pebbleEngine);
 	}
 
-	private Path getTemplateDirectory(String templateName)
-	{
+	private Path getTemplateDirectory() {
 		ClassLoader classLoader = getClass().getClassLoader();
-		URL resource = classLoader.getResource(TEST_TEMPLATE_DIRECTORY + "/" + templateName);
+		URL resource = classLoader.getResource(TEST_TEMPLATE_DIRECTORY);
 
 		assertThat(resource)
-				.as("Template " + templateName + " not found")
+				.as("Template directory " + TEST_TEMPLATE_DIRECTORY + " not found")
 				.isNotNull();
 
-		return Path.of(resource.getPath()).getParent();
+		return Path.of(resource.getPath());
 	}
 
-	private static class Context
-	{
+	private static class Context {
 		public String name = "John Doe";
 		public Optional<String> optional = Optional.empty();
 	}
