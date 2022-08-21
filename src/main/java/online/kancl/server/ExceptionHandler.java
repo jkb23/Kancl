cc0f9627fce5173cdd61d0c56fc6358ee8ce1e4d
Simonek, Jan <jsimonek@netsuite.com>
2022-08-21 06:58:09 +0200

    Logging exceptions to System.err so they are visible in the output of e2e tests

--- src/main/java/online/kancl/server/ExceptionHandler.java
+++ src/main/java/online/kancl/server/ExceptionHandler.java
@@ -8,19 +8,33 @@ import java.io.StringWriter;
 
 public class ExceptionHandler {
 
-	public void handleException(Exception exception, Request request, Response response) {
-		response.status(500);
+	private static final String RESET_COLOR = "\u001B[0m";
+	private static final String RED = "\u001B[31m";
 
+	public void handleException(Exception exception, Request request, Response response) {
+		logExceptionInRequest(exception, request);
+
+		response.status(500);
 		response.body(renderException(exception));
 	}
 
+	private void logExceptionInRequest(Exception exception, Request request) {
+		System.out.println(
+				RED + "Exception when handling " + request.requestMethod() + " to " + request.url() + RESET_COLOR + "\n"
+				+ getExceptionText(exception) + "\n"
+		);
+	}
+
 	private String renderException(Exception exception) {
+		return "<h1>Unexpected error</h1>"
+				+ "<pre>" + getExceptionText(exception) + "</pre>";
+	}
+
+	private String getExceptionText(Exception exception) {
 		var stringWriter = new StringWriter();
 		var printWriter = new PrintWriter(stringWriter);
 
 		exception.printStackTrace(printWriter);
-
-		return "<h1>Unexpected error</h1>"
-				+ "<pre>" + stringWriter + "</pre>";
+		return stringWriter.toString();
 	}
 }
