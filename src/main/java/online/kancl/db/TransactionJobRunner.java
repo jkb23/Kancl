8adcf32fb733d3fce86f559a6e6d04e3e7288bd7
Simonek, Jan <jsimonek@netsuite.com>
2022-08-22 15:15:45 +0200

    extracted method TransactionJobRunner.runTransactionInConnection

--- src/main/java/online/kancl/db/TransactionJobRunner.java
+++ src/main/java/online/kancl/db/TransactionJobRunner.java
@@ -10,24 +10,28 @@ public class TransactionJobRunner {
 
 	public static <T> T runInTransactionAndRelease(Function<DatabaseRunner, T> job) {
 		try (Connection connection = Main.getConnection()) {
-			Boolean originalAutoCommit = null;
-			try {
-				originalAutoCommit = connection.getAutoCommit();
-
-				var databaseRunner = new DatabaseRunner(connection);
-				T result = job.apply(databaseRunner);
-
-				connection.commit();
-
-				return result;
-			} catch (Exception e) {
-				rollback(connection);
-				throw e;
-			} finally {
-				restoreAutocommit(connection, originalAutoCommit);
-			}
+			return runTransactionInConnection(job, connection);
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
+		}
+	}
+
+	private static <T> T runTransactionInConnection(Function<DatabaseRunner, T> job, Connection connection) throws SQLException {
+		Boolean originalAutoCommit = null;
+		try {
+			originalAutoCommit = connection.getAutoCommit();
+
+			var databaseRunner = new DatabaseRunner(connection);
+			T result = job.apply(databaseRunner);
+
+			connection.commit();
+
+			return result;
+		} catch (Exception e) {
+			rollback(connection);
+			throw e;
+		} finally {
+			restoreAutocommit(connection, originalAutoCommit);
 		}
 	}
 
