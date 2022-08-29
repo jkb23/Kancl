914c2ebb6cc14227f3973c9bd338c44e5262fd7e
Simonek, Jan <jsimonek@netsuite.com>
2022-08-29 10:59:23 +0200

    Fixed typo

--- src/test/java/online/kancl/db/DirectoryHashCalculatorTest.java
+++ src/test/java/online/kancl/db/DirectoryHashCalculatorTest.java
@@ -10,23 +10,23 @@ class DirectoryHashCalculatorTest {
 
 	@Test
 	void sameFileNameSameContent_sameHash() {
-		Assertions.assertThat(hasForDirectory("hashCalculator/dirWithFileAContentBar"))
-				.isEqualTo(hasForDirectory("hashCalculator/anotherDirWithFileAContentBar"));
+		Assertions.assertThat(hashForDirectory("hashCalculator/dirWithFileAContentBar"))
+				.isEqualTo(hashForDirectory("hashCalculator/anotherDirWithFileAContentBar"));
 	}
 
 	@Test
 	void sameFileNameDifferentContent_differentHash() {
-		Assertions.assertThat(hasForDirectory("hashCalculator/dirWithFileAContentBar"))
-				.isNotEqualTo(hasForDirectory("hashCalculator/dirWithFileAContentFoo"));
+		Assertions.assertThat(hashForDirectory("hashCalculator/dirWithFileAContentBar"))
+				.isNotEqualTo(hashForDirectory("hashCalculator/dirWithFileAContentFoo"));
 	}
 
 	@Test
 	void differentFileNameSameContent_differentHash() {
-		Assertions.assertThat(hasForDirectory("hashCalculator/dirWithFileAContentBar"))
-				.isNotEqualTo(hasForDirectory("hashCalculator/dirWithFileBContentBar"));
+		Assertions.assertThat(hashForDirectory("hashCalculator/dirWithFileAContentBar"))
+				.isNotEqualTo(hashForDirectory("hashCalculator/dirWithFileBContentBar"));
 	}
 
-	private String hasForDirectory(String directoryName) {
+	private String hashForDirectory(String directoryName) {
 		return hashCalculator.calculateEncodedHash(getResourcePath(this, directoryName));
 	}
 }
