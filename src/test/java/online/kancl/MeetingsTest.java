4b6a4bd74ff90bff786741f2b31992f79687670e
Simonek, Jan <jsimonek@netsuite.com>
2022-08-30 12:30:31 +0200

    Reformatted MeetingsTest

--- src/test/java/online/kancl/MeetingsTest.java
+++ src/test/java/online/kancl/MeetingsTest.java
@@ -5,25 +5,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MeetingsTest
-{
+class MeetingsTest {
+
 	private final Meetings meetings = new Meetings();
 
 	@Test
-	void initially_meetingIsEmpty()
-	{
+	void initially_meetingIsEmpty() {
 		assertThat(meetings.getJoinedParticipantName())
 				.isEmpty();
 	}
 
 	@Test
-	void joinedParticipant_isInMeeting()
-	{
+	void joinedParticipant_isInMeeting() {
 		meetings.participantJoined("John Doe");
 
 		assertThat(meetings.getJoinedParticipantName())
 				.as("Participant name")
-				.get()
-				.isEqualTo("John Doe");
+				.contains("John Doe");
 	}
 }
