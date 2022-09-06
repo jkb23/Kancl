591d5c4dcaa95fb193290fe97a6f370f8bead42b
summercamp2022 <ratemal@seznam.cz>
2022-09-06 11:19:03 +0200

    refactoring

--- src/main/java/online/kancl/auth/Auth.java
+++ src/main/java/online/kancl/auth/Auth.java
@@ -9,60 +9,45 @@ import static online.kancl.auth.AuthReturnCode.*;
 
 public class Auth {
 
-    public static final int FIVE_MINUTES = 5 * 60 * 1000;
+    public static final int BLOCKED_DURATION_IN_MILLISECONDS = 5 * 60 * 1000;
 
-    /**
-     * checks whether the user is able to log in and if he's using the correct credentials.
-     * If the credentials of a particular user were incorrect 5 times in a row, the user is blocked from logging in
-     * for 5 minutes
-     * @param username username specified in login
-     * @param password password specified in login
-     * @return enum describing the return code
-     */
-    public AuthReturnCode checkCredentials(String username, String password){
-        String hashedPassword;
-
-        if(isBlocked(username))
-        {
+    public AuthReturnCode checkCredentialsWithBruteForcePrevention(String username, String password) {
+        if (isBlocked(username)) {
             return BLOCKED_USER;
         }
 
-        hashedPassword = HashUtils.sha256Hash(password);
+        String hashedPassword = HashUtils.sha256Hash(password);
+        boolean validCredentials = UserStorage.findUser(username, hashedPassword);
 
-        if(!UserStorage.findUser(username,hashedPassword))
-        {
-            UserStorage.setBadLoginCnt(username, UserStorage.getBadLoginCnt(username) + 1);
-            if (UserStorage.getBadLoginCnt(username) >= 5)
-            {
-                blockUser(username);
-            }
-
+        if (validCredentials) {
+            clearBadLoginCount(username);
+            return CORRECT;
+        } else {
+            preventBruteForce(username);
             return BAD_CREDENTIALS;
         }
-        //if the login is successful, clears the bad login count
-        UserStorage.setBadLoginCnt(username, 0);
-
-        return CORRECT;
     }
 
-    /**
-     * Sets the timestamp of when the user was blocked from logging in
-     * @param username user to block
-     */
-    private static void blockUser(String username)
-    {
+    private void clearBadLoginCount(String username) {
+        UserStorage.setBadLoginCnt(username, 0);
+    }
+
+    private void preventBruteForce(String username) {
+        int badLoginCount = UserStorage.getBadLoginCnt(username) + 1;
+
+        UserStorage.setBadLoginCnt(username, badLoginCount);
+        if (badLoginCount >= 5) {
+            blockUser(username);
+        }
+    }
+
+    private void blockUser(String username) {
         UserStorage.setBadLoginTimestamp(username, new Timestamp(System.currentTimeMillis()));
     }
 
-    /**
-     * Checks if the user were blocked from logging in within the last 5 minutes
-     * @param username the user being checked
-     * @return true if the user was blocked, false otherwise
-     */
-    public static boolean isBlocked(String username)
-    {
+    static boolean isBlocked(String username) {
         Timestamp badLogin = UserStorage.getBadLoginTimestamp(username);
-        badLogin.setTime(badLogin.getTime() + FIVE_MINUTES);
+        badLogin.setTime(badLogin.getTime() + BLOCKED_DURATION_IN_MILLISECONDS);
         Timestamp currentTime = new Timestamp(System.currentTimeMillis());
 
         return currentTime.before(badLogin);
