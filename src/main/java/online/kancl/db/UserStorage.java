88a6885dc4c36025c3475dfdba8ea5d87eb32554
Martin Homola <mhomola1@gmail.com>
2022-09-06 12:48:01 +0200

    UserStorage metods changed to static, repair tests

--- src/main/java/online/kancl/db/UserStorage.java
+++ src/main/java/online/kancl/db/UserStorage.java
@@ -4,14 +4,14 @@ import java.sql.Timestamp;
 
 public class UserStorage {
 
-    public boolean findUser(DatabaseRunner dbRunner, String username, String hash) {
+    public static boolean findUser(DatabaseRunner dbRunner, String username, String hash) {
          int isFound = dbRunner.selectInt(
                 "SELECT COUNT(*) FROM AppUser WHERE username= ? AND password = ?",
                 username, hash);
         return isFound == 1;
     }
 
-    public void createUser(DatabaseRunner dbRunner, String username, String hash) {
+    public static void createUser(DatabaseRunner dbRunner, String username, String hash) {
         try {
             dbRunner.insert("INSERT INTO AppUser (username, password, nickname, avatar, avatar_color, bad_login_count, bad_login_timestamp)" +
                     " VALUES(?, ?, null, null, null, null, null)", username, hash);
@@ -26,29 +26,29 @@ public class UserStorage {
         }
     }
 
-    public int getBadLoginCount(DatabaseRunner dbRunner, String username) {
+    public static int getBadLoginCount(DatabaseRunner dbRunner, String username) {
         return dbRunner.selectInt("SELECT bad_login_count FROM AppUser WHERE username= ?",
                 username);
     }
 
-    public void incrementBadLoginCount(DatabaseRunner dbRunner, String username) {
+    public static void incrementBadLoginCount(DatabaseRunner dbRunner, String username) {
         int BadLoginCount = dbRunner.selectInt("SELECT bad_login_count FROM AppUser WHERE username= ?",
                 username);
         dbRunner.update("UPDATE AppUser SET bad_login_count = ? WHERE username= ?",
                 BadLoginCount + 1, username);
     }
 
-    public void nullBadLoginCount(DatabaseRunner dbRunner, String username) {
+    public static void nullBadLoginCount(DatabaseRunner dbRunner, String username) {
         dbRunner.update("UPDATE AppUser SET bad_login_count = 0 WHERE username= ?",
                 username);
     }
 
-    public void setBadLoginTimestamp(DatabaseRunner dbRunner, String username, Timestamp timestamp) {
+    public static void setBadLoginTimestamp(DatabaseRunner dbRunner, String username, Timestamp timestamp) {
         dbRunner.update("UPDATE AppUser SET bad_login_timestamp = ? WHERE username= ?",
                 timestamp, username);
     }
 
-    public Timestamp getBadLoginTimestamp(DatabaseRunner dbRunner, String username) {
+    public static Timestamp getBadLoginTimestamp(DatabaseRunner dbRunner, String username) {
         return dbRunner.select("SELECT bad_login_timestamp FROM AppUser WHERE username= ?",
                 (r) -> r.getTimestamp(1),
                 username).orElseThrow();
