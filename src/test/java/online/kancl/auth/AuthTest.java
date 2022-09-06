26c0b9186836db14718f69e57b520f96dd106790
jkb23 <jkb.1327@gmail.com>
2022-09-06 11:55:15 +0200

    new login tests added

--- src/test/java/online/kancl/auth/AuthTest.java
+++ src/test/java/online/kancl/auth/AuthTest.java
@@ -1,14 +1,40 @@
 package online.kancl.auth;
 
-import online.kancl.page.users.UserStorage;
+import online.kancl.auth.Auth;
+import online.kancl.auth.AuthReturnCode;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AuthTest {
+class AuthTest {
+
     @Test
-    void check5minTimeout(){
-        UserStorage.createUser("ahoj", "");
-        assertThat(Auth.isBlocked("ahoj")).isEqualTo(false);
+    void credentialsTest() {
+        Auth auth = new Auth();
+        assertThat(auth.checkCredentialsWithBruteForcePrevention("correct", "correct"))
+                .isEqualTo(AuthReturnCode.CORRECT);
+
+        assertThat(auth.checkCredentialsWithBruteForcePrevention("incorrect", "correct"))
+                .isEqualTo(AuthReturnCode.BAD_CREDENTIALS);
+
+        assertThat(auth.checkCredentialsWithBruteForcePrevention("correct", "incorrect"))
+                .isEqualTo(AuthReturnCode.BAD_CREDENTIALS);
+
+        assertThat(auth.checkCredentialsWithBruteForcePrevention("incorrect", "incorrect"))
+                .isEqualTo(AuthReturnCode.BAD_CREDENTIALS);
     }
+
+    @Test
+    void blockTest() {
+        Auth auth = new Auth();
+
+        for (int i = 0; i < 5; i++){
+            assertThat(auth.checkCredentialsWithBruteForcePrevention("block", "block"))
+                    .isEqualTo(AuthReturnCode.BAD_CREDENTIALS);
+        }
+
+        assertThat(auth.checkCredentialsWithBruteForcePrevention("block", "block"))
+                .isEqualTo(AuthReturnCode.BLOCKED_USER);
+    }
+
 }
