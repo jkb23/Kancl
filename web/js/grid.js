fba2575208c8a7965478d9cc0f2d991c5d92458c
summercamp2022 <ratemal@seznam.cz>
2022-09-07 13:40:15 +0200

    added function to check obstacles

--- web/js/grid.js
+++ web/js/grid.js
@@ -1,5 +1,21 @@
 let user = document.getElementById("user");
 const array = [];
+obstacles = [][2];
+
+function check_obstacles(x,y)
+{
+    for(i = 0; i != obstacles.length(); i++)
+    {
+        if(obstacles[i][0] == x)
+        {
+            if(obstacles[i][1] == y)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 let container = document.getElementById('container');
 for (let i = 0; i < 468; i++) {
