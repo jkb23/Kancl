e57efe2a7c9917d432cc5ddc0f75843d04b5b706
jkb23 <jkb.1327@gmail.com>
2022-09-08 09:44:53 +0200

    added user status

--- src/main/java/online/kancl/objects/Wall.java
+++ src/main/java/online/kancl/objects/Wall.java
@@ -5,7 +5,4 @@ public class Wall extends OfficeObject {
     public Wall(int x, int y) {
         super(x, y);
     }
-
-    public void moveWall(int x, int y){
-    }
 }
--- src/main/java/online/kancl/objects/ZoomObject.java
+++ src/main/java/online/kancl/objects/ZoomObject.java
@@ -1,7 +1,10 @@
 package online.kancl.objects;
 
 public class ZoomObject extends OfficeObject {
-    public ZoomObject(int x, int y) {
+
+    private String zoomLink;
+    public ZoomObject(int x, int y, String zoomLink) {
         super(x, y);
+        this.zoomLink = zoomLink;
     }
 }
