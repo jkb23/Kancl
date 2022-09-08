ab72d839b5e803e61c4b0d7490be0d016ef0f0cf
Simonek, Jan <jsimonek@netsuite.com>
2022-09-08 15:31:40 +0200

    commit

--- src/test/cypress/e2e/loginPage.ts
+++ src/test/cypress/e2e/loginPage.ts
@@ -6,51 +6,51 @@ describe('Login page', () => {
     })
 
     it("enters correct credentials", () => {
-        application.openLoginPage()
-        application.enterCredentials('correct', 'correct')
-        application.clickSubmit()
-        application.doesNotShowInvalidCredentials()
-        application.doesNotDisplayLoginPage()
+        application.openLoginPage();
+        application.enterCredentials('correct', 'correct');
+        application.clickSubmit();
+        application.doesNotShowInvalidCredentials();
+        application.doesNotDisplayLoginPage();
     });
 
     it("enters incorrect username", () => {
-        application.openLoginPage()
-        application.enterCredentials('incorrect', 'correct')
-        application.clickSubmit()
-        application.displaysInvalidCredentials()
-        application.displaysLoginPage()
+        application.openLoginPage();
+        application.enterCredentials('incorrect', 'correct');
+        application.clickSubmit();
+        application.displaysInvalidCredentials();
+        application.displaysLoginPage();
     });
 
     it("enters incorrect password", () => {
-        application.openLoginPage()
-        application.enterCredentials('correct', 'incorrect')
-        application.clickSubmit()
-        application.displaysInvalidCredentials()
-        application.displaysLoginPage()
+        application.openLoginPage();
+        application.enterCredentials('correct', 'incorrect');
+        application.clickSubmit();
+        application.displaysInvalidCredentials();
+        application.displaysLoginPage();
     });
 
     it("leaves username empty", () => {
-        application.openLoginPage()
-        application.enterCredentials('', 'correct')
-        application.submitButtonIsDisabled()
+        application.openLoginPage();
+        application.enterCredentials('', 'correct');
+        application.submitButtonIsDisabled();
     });
 
     it("leaves password empty", () => {
-        application.openLoginPage()
-        application.enterCredentials('user', '')
-        application.submitButtonIsDisabled()
-        application.displaysLoginPage()
+        application.openLoginPage();
+        application.enterCredentials('user', '');
+        application.submitButtonIsDisabled();
+        application.displaysLoginPage();
     });
 
     it("enters incorrect credentials 5 times", () => {
-        application.openLoginPage()
+        application.openLoginPage();
 
         for (let i = 0; i < 6; i++) {
-            application.enterCredentials('correct', 'password');
-            application.clickSubmit()
+            application.enterCredentials('correct', 'password')
+            application.clickSubmit();
         }
 
-        application.displaysTooManyUnsuccessfulLoginAttempts()
-        application.displaysLoginPage()
+        application.displaysTooManyUnsuccessfulLoginAttempts();
+        application.displaysLoginPage();
     });
 });
