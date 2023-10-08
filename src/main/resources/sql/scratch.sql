a689e30815d97c5ef269aab39d6196230faa4055
mjakab <514495@mail.muni.cz>
2023-10-08 19:34:46 +0200

    fixing db bug from previous commit

--- src/main/resources/sql/scratch.sql
+++ src/main/resources/sql/scratch.sql
@@ -3,13 +3,6 @@ CREATE TABLE DatabaseSchemaHash
     hash VARCHAR(300)
 );
 
-CREATE TABLE Comment
-(
-    id      INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    author  VARCHAR(50)  NOT NULL,
-    message VARCHAR(300) NOT NULL
-);
-
 CREATE TABLE AppUser
 (
     id                  INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -20,8 +13,8 @@ CREATE TABLE AppUser
     bg_color            VARCHAR(50),
     bad_login_count     INTEGER DEFAULT 0,
     bad_login_timestamp TIMESTAMP,
-    user_status         VARCHAR(200),
+    user_status         VARCHAR(200)
 );
 
-INSERT INTO AppUser (username, password, email, bg_color, user_status, profile_picture)
+INSERT INTO AppUser (username, password, email, bg_color, user_status)
       VALUES('correct', '15a596e3c98c407e043751ff3b21ff0358a1bdfdf3fe948b1523893a8e5de2e8', 'neexistuju@baf.com', 'blue', 'Mam se dobre!')
